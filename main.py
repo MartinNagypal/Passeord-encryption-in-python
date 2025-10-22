5e9927903cf4a4634fb62a8bdfb6a9fb9284e683
MartinNagypal <martinbeka8@gmail.com>
2025-10-22 21:22:25 +0200

    cli login/register system with DB intergration

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
+import sqlite3
 from argon2 import PasswordHasher
+from datetime import datetime
 
-ph = PasswordHasher() #create ph object
+ph = PasswordHasher()
 
-print("\n enter username: ")
+print("\nlogin/register: ")
+mode = input()
+print("\nEnter username: ")
 username = input()
-print("\n enter password: ")
-pw = input()
+print("\nEnter password: ")
+password = input()
 
-def hashPw(password):
-    hashedPw = ph.hash(password) #encrypt with argon2 parameters
-    return hashedPw
+conn = sqlite3.connect("user.db")
+cursor = conn.cursor()
+cursor.execute(""" 
+CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE,
+        passwordHashe TEXT,
+        createdAt TEXT
+               )
+""")
 
-print(f'\n encrypted PW: \n{hashPw(pw)}')
+conn.commit()
 
-print(f'\nlogin verify: {ph.verify(hashPw(pw), pw)}\n')
+def createUser(username:str, password:str):
+    passwordHash = ph.hash(password)
+    print("\n" + passwordHash)
+    cursor.execute("SELECT username FROM user WHERE username = ?", (username,))
+    row = cursor.fetchone()
+    if row:
+        return "User already exists"
+    else:
+        try:
+            cursor.execute("INSERT INTO user (username, passwordHash, createdAt) VALUES (?,?,?)", (username, passwordHash, datetime.now().isoformat(),))
+            conn.commit()
+            return "User created"
+        except Exception as e:
+            return e
+    
+def verifyUser(username:str, password:str):
+    cursor.execute("SELECT passwordHash FROM user WHERE username = ?", (username,))
+    row = cursor.fetchone()
+    if not row:
+        return f'No account with username: {username}'
+    hash = row[0]
+    try:
+        if(ph.verify(hash, password) == True):
+            return "Sucsessfully logged in"
+        else:
+            return "Wrong password or username"
+    except Exception as e:
+        return e
+
+
+if(mode == "register"):
+    print(createUser(username, password))
+elif(mode == "login"):
+    print(verifyUser(username, password))
+else:
+    pass
